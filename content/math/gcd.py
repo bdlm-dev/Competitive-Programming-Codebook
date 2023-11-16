a5dab64a233d3d75676f41a87546a0d77e0f0414
bdlm-dev <bdlm@hotmail.co.uk>
2023-11-16 23:49:24 +0000

    Implement gcd of list of numbers

--- content/math/gcd.py
+++ content/math/gcd.py
@@ -1,1 +1,11 @@
 from math import gcd
+# gcd of multiple numbers:
+def gcd_m(lst):
+    if len(lst) == 0:
+        return 999999 # no gcd
+    elif len(lst) == 1:
+        return lst[0]
+    out = gcd(lst[0], lst[1])
+    for i in range(2, len(lst)):
+        out = gcd(out, lst[i])
+    return out
